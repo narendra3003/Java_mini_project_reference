2df2a14e60e4cae93b8faee33b960f0372a8ba5f
narendra3003 <narendradukhande30@gmail.com>
2023-09-16 12:59:27 +0530

    updated select file

--- Z_Select.java
+++ Z_Select.java
@@ -4,25 +4,23 @@ import java.sql.*;
 
 public class Z_Select
 {
-	public static void main(String args[])
-	{
-		String id = "id1";
-		String pwd = "pwd1";
+	public static void PrintSelected(String id){
 		try
 		{
-			Class.forName("oracle.jdbc.driver.OracleDriver");
-			Connection con = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:orcl", "login1", "pwd1");
+			Class.forName("com.mysql.jdbc.Driver");
+			Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/Expensedb", "root", "oracle");
 			Statement stmt = con.createStatement();
 			
 			// SELECT query
-			String q1 = "select * from userid WHERE id = '" + id +
-									"' AND pwd = '" + pwd + "'";
+			String q1 = "select * from expenses WHERE idexpenses = '" + id + "';";
+			// String q1 = "select * from userid WHERE id = '" + id +
+			// 						"' AND pwd = '" + pwd + "'";
 			ResultSet rs = stmt.executeQuery(q1);
 			if (rs.next())
 			{
-				System.out.println("User-Id : " + rs.getString(1));
-				System.out.println("Full Name :" + rs.getString(3));
-				System.out.println("E-mail :" + rs.getString(4));
+				System.out.println("Expense -Id : " + rs.getString(1));
+				System.out.println("Name :" + rs.getString(2));
+				System.out.println("Amount :" + rs.getString(3));
 			}
 			else
 			{
@@ -35,5 +33,10 @@ public class Z_Select
 			System.out.println(e);
 		}
 	}
+	public static void main(String args[])
+	{
+		PrintSelected("102");
+	}
+		
 }
 
