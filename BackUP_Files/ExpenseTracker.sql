cc444d7cbb9dc2afee111956d11f1ece33f66e8b
narendra3003 <narendradukhande30@gmail.com>
2023-10-03 15:39:33 +0530

    added Budget Display

--- BackUP_Files/ExpenseTracker.sql
+++ BackUP_Files/ExpenseTracker.sql
@@ -10,8 +10,8 @@ insert into Expense_category values(301,'education','101'),(302,'Transportation'
 create table transactions(transaction_id int(5),description varchar(20),transactiondate DATE,amount int(4),Ecategory_id int(2),user_id int(2),foreign key (Ecategory_id) references Expense_category(Ecategory_id),foreign key (user_id) references User(user_id));
 insert into transactions values(401,'transaction details','2023-08-17',40000,301,101),(402,'transaction details','2023-08-18',20000,302,102);
 
-create table Transaction_category(category_id int(2),categoryname varchar(20),description varchar(50));
-insert into transaction_category values(201,'xyz','Online payment'),(202,'abc','Offline payment');
+create table Transaction_category(category_id int(2),categoryname varchar(20),description varchar(50), categLimit int(10));
+insert into transaction_category values(201,'xyz','Online payment', 50000),(202,'abc','Offline payment', 20000);
 
 create table Savings(saving_id int(2)primary key,description varchar(50),savingsdate DATE,category_id int(2),User_id int(2),foreign key(User_id) references User(User_id));
 insert into savings values(101,'Details','2023-09-18',201,101),(102,'Other details','2023-09-17',202,102);
---
+++ main/Budget.java
@@ -0,0 +1,16 @@
+package main;
+public class Budget{
+    private String categ;
+    private int limit;
+
+    public Budget(String categ, int limit){
+        this.categ=categ;
+        this.limit=limit;
+    }
+    public String getCateg(){
+        return this.categ;
+    }
+    public int getLimit(){
+        return this.limit;
+    }
+}
